e33740d308b4ae2436fc60b90fe46730688f8195
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-08-24 13:48:02 +0500

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
   const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
   return (
     <html lang="en" suppressHydrationWarning>
@@ -144,8 +145,31 @@ html {
         />
       </head>
       <body className={`${GeistSans.variable} ${manrope.variable} antialiased bg-background text-foreground`}>
-        {/* Google Analytics 4 */}
-        {GA_ID && (
+        {/* Google Tag Manager (preferred) */}
+        {GTM_ID ? (
+          <>
+            <Script id="gtm-init" strategy="afterInteractive">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `}
+            </Script>
+            {/* GTM noscript fallback */}
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+              />
+            </noscript>
+          </>
+        ) : (
+          /* Google Analytics 4 */
+          GA_ID && (
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
@@ -160,6 +184,7 @@ html {
               `}
             </Script>
           </>
+          )
         )}
         {/* Skip to content for keyboard users */}
         <a
